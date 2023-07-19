708a729253fbf1548c5db69b1d3548662bfaab5c
Adrian Ramos <airria2000@gmail.com>
2023-07-18 21:03:44 -0500

    Firebase Function Server Connected

--- screens/Scan.js
+++ screens/Scan.js
@@ -1,10 +1,34 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { initializeApp } from 'firebase/app';
+import { getFunctions, httpsCallable } from 'firebase/functions';
+import { firebaseConfig } from '../firebaseConfig'
+
+const app = initializeApp(firebaseConfig);
+const functions = getFunctions(app);
 
 const Scan = () => {
+  const handleClick = () => {
+    // Invoke the Cloud Function
+
+  const addMessage = httpsCallable(functions, 'helloWorld');
+  addMessage()
+    .then((result) => {
+      console.log(result.data); // Should log "Hello from Firebase!"
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  };
+
   return (
     <View>
       <Text>Scan</Text>
+      <Button 
+      title="test" 
+      color="red" 
+      onPress={handleClick}
+      />
     </View>
   )
 }
