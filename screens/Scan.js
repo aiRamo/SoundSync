e6a96e3dc2980c12e0e95ac3b72a484329af6a03
Benjamin <benfarm17@gmail.com>
2023-07-20 18:46:42 -0500

    note stuff

--- screens/Scan.js
+++ screens/Scan.js
@@ -1,21 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Image, View, Platform } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import { initializeApp } from 'firebase/app';
-import { getFunctions, httpsCallable } from 'firebase/functions';
-import { firebaseConfig } from '../firebaseConfig'
+import React, { useState, useEffect } from "react";
+import { Button, Image, View, Platform, Text } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { initializeApp } from "firebase/app";
+import { getFunctions, httpsCallable } from "firebase/functions";
+import { firebaseConfig } from "../firebaseConfig";
 
 const app = initializeApp(firebaseConfig);
 const functions = getFunctions(app);
 
 export default function Scan() {
+  let temp = "";
+  const [holder, setHolder] = useState("");
   const [image, setImage] = useState(null);
   const [imageBlob, setImageBlob] = useState(null);
   const [inputFile, setInputFile] = useState(null);
 
   function dataURItoBlob(dataURI) {
-    const byteString = atob(dataURI.split(',')[1]);
-    const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    const byteString = atob(dataURI.split(",")[1]);
+    const mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
     const ab = new ArrayBuffer(byteString.length);
     const ia = new Uint8Array(ab);
     for (let i = 0; i < byteString.length; i++) {
@@ -31,7 +33,7 @@ export default function Scan() {
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
-      base64: false
+      base64: false,
     });
 
     if (!result.canceled) {
@@ -40,49 +42,57 @@ export default function Scan() {
   };
 
   useEffect(() => {
-    if (image){
+    if (image) {
       setImageBlob(dataURItoBlob(image));
     }
   }, [image]);
 
   useEffect(() => {
-    if (imageBlob){
-      setInputFile(new File([imageBlob], 'example.png', { type: 'image/png' }));
+    if (imageBlob) {
+      setInputFile(new File([imageBlob], "example.png", { type: "image/png" }));
       console.log("Successfully set Input File.");
     }
   }, [imageBlob]);
 
-  
-  
   const handleClick = async () => {
     try {
-      const apiUrl = 'http://192.168.208.1:3000/upload'; // Replace with your locally hosted API URL
+      const apiUrl = "http://192.168.86.50:3000/upload"; // Replace with your locally hosted API URL
       const formData = new FormData();
-      formData.append('file', inputFile); // Use the 'file' variable instead of 'image'
-  
+      formData.append("file", inputFile); // Use the 'file' variable instead of 'image'
+
       const response = await fetch(apiUrl, {
-        method: 'POST',
+        method: "POST",
         body: formData,
       });
-  
+
       if (response.ok) {
         const notesJson = await response.json();
-        console.log('Notes JSON:', notesJson);
+        console.log("Notes JSON:", notesJson);
+
+        for (var i = 0; i < notesJson.notes[0].notes.length; i++) {
+          temp += notesJson.notes[0].notes[i].pitch[0].step[0];
+          temp += notesJson.notes[0].notes[i].pitch[0].octave[0];
+          temp += ",";
+        }
+        setHolder(temp);
+
         // Now you have the notes JSON, and you can process it further as needed.
       } else {
-        console.log('API call failed:', response.statusText);
+        console.log("API call failed:", response.statusText);
       }
     } catch (error) {
-      console.error('Error calling API:', error);
+      console.error("Error calling API:", error);
     }
   };
-  
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Button title="Pick image" onPress={pickImage} />
-       <Button title="test" color="red" onPress={handleClick} />
-      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      <Button title="test" color="red" onPress={handleClick} />
+      {image && (
+        <Image source={{ uri: image }} style={{ height: 200, width: 600 }} />
+      )}
+      <Text>{holder}</Text>
     </View>
   );
 }
