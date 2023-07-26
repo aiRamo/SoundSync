4345f61d55d8590bd64b349bd3cee83395a648c3
Benjamin <benfarm17@gmail.com>
2023-07-26 16:09:10 -0500

    test

--- screens/Audio.js
+++ screens/Audio.js
@@ -1,15 +1,20 @@
-import { Audio } from "expo-av"
+import { Audio } from "expo-av";
 import { Button, Image, View, ScrollView, Text } from "react-native";
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react";
 
 const AudioRecorder = () => {
-  const [currentNote, setCurrentNote] = useState(""); /*value of note to be displayed*/
-  const [isRecording, setIsRecording] = useState(false); /*for demo will record only when selected*/
+  const [currentNote, setCurrentNote] =
+    useState(""); /*value of note to be displayed*/
+  const [isRecording, setIsRecording] =
+    useState(false); /*for demo will record only when selected*/
   const [recorder, setRecorder] = useState(null); /*recorder object*/
 
-  useEffect (() => {      /*request mic permission on first load*/
+  //test
+
+  useEffect(() => {
+    /*request mic permission on first load*/
     (async () => {
-      try{
+      try {
         await Audio.requestPermissionsAsync();
       } catch (error) {
         console.error("Need audio permission:", error);
@@ -17,22 +22,24 @@ const AudioRecorder = () => {
     })();
   }, []);
 
-  const toggleRecording = () => {   /*starts and stops recording loop*/
+  const toggleRecording = () => {
+    /*starts and stops recording loop*/
     if (isRecording) {
       setIsRecording(false);
       clearTimeout(recordingLoop);
-    }
-    else {
+    } else {
       setIsRecording(true);
       recordingLoop();
     }
-  }
+  };
 
-  const recordingLoop = async () => {   
-    if(isRecording) {
+  const recordingLoop = async () => {
+    if (isRecording) {
       try {
         const recording = new Audio.Recording();
-        await recording.prepareToRecordAsync(Audio.RecordingOptionsPresets.LOW_QUALITY);
+        await recording.prepareToRecordAsync(
+          Audio.RecordingOptionsPresets.LOW_QUALITY
+        );
         await recording.startAsync();
         setIsRecording(recording);
 
@@ -42,22 +49,26 @@ const AudioRecorder = () => {
           const { sound } = await Audio.Sound.createAsync({ uri });
           recordingLoop();
         }, 200);
-      }
-      catch (error) {
+      } catch (error) {
         console.error("rec error", error);
       }
     }
   };
 
-  return (        /*recording toggle button*/
+  return (
+    /*recording toggle button*/
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <ScrollView style={{ width: '100%' }}>
-        <View style={{ width: '30%', alignSelf: 'center', marginBottom: 10}}>
-          <Button title={isRecording ? "Stop Recording" : "Record"} color ={isRecording ? "red" : "green"} onPress={toggleRecording} />
+      <ScrollView style={{ width: "100%" }}>
+        <View style={{ width: "30%", alignSelf: "center", marginBottom: 10 }}>
+          <Button
+            title={isRecording ? "Stop Recording" : "Record"}
+            color={isRecording ? "red" : "green"}
+            onPress={toggleRecording}
+          />
         </View>
       </ScrollView>
     </View>
   );
-}
+};
 
 export default AudioRecorder;
