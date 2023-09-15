bfc1f96ad71021fdb373d608f7a45c7fd584383a
Benjamin <benfarm17@gmail.com>
2023-09-15 14:28:20 -0500

    search funct

--- screens/Library.js
+++ screens/Library.js
@@ -1,13 +1,20 @@
-import { View, Text, ScrollView, StatusBar, StyleSheet } from "react-native";
 import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  TextInput,
+} from "react-native";
 import { ref, getDownloadURL, listAll } from "firebase/storage";
 import { STORAGE } from "../firebaseConfig";
 import Card from "../components/Card";
-import { Ionicons } from "@expo/vector-icons";
+import { Entypo, AntDesign } from "@expo/vector-icons";
 
 export default function Library() {
   const [imageUrls, setImageUrls] = useState([]);
-  let count = 0;
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     async function listFilesInFolder(folderPath) {
@@ -30,20 +37,49 @@ export default function Library() {
     listFilesInFolder("images/eP42yMFVDcdvkwroV3OrFv4yutH2/BenTestFolder/");
   }, []);
 
+  const filteredImageUrls = imageUrls.filter((imageUrl, index) =>
+    `music page ${index}`.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
-    <View style={styles.container}>
-      <View>
-        <Ionicons name="filter-sharp" size={30} color="black" />
+    <View style={{ flex: 1 }}>
+      <View
+        style={{
+          backgroundColor: "darkslateblue",
+        }}
+      >
+        <View
+          style={{
+            marginTop: StatusBar.currentHeight,
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <TextInput
+            placeholder="Search"
+            placeholderTextColor="white"
+            clearButtonMode="always"
+            style={styles.search}
+            onChangeText={(text) => setSearchQuery(text)}
+          />
+          <Entypo name="dots-three-vertical" size={24} color="white" />
+        </View>
       </View>
-      <ScrollView>
-        {imageUrls.map((imageUrl, index) => (
-          <Card
-            key={index}
-            imgeUrl={imageUrl}
-            title={`music page ${count++}`}
-          ></Card>
-        ))}
-      </ScrollView>
+      <View style={styles.container}>
+        <View style={{ marginBottom: 10, flexDirection: "row" }}>
+          <AntDesign name="filter" size={24} color="black" />
+          <Text>Descending Order</Text>
+        </View>
+        <ScrollView>
+          {filteredImageUrls.map((imageUrl, index) => (
+            <Card
+              key={index}
+              imgeUrl={imageUrl}
+              title={`music page ${index}`}
+            ></Card>
+          ))}
+        </ScrollView>
+      </View>
     </View>
   );
 }
@@ -53,8 +89,18 @@ const styles = StyleSheet.create({
     flex: 1,
     alignContent: "center",
     justifyContent: "center",
+    marginTop: 10,
     marginLeft: 10,
     marginRight: 10,
-    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    flex: 1,
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    borderColor: "white",
+    color: "white",
+    marginBottom: 5,
+    marginLeft: 10,
   },
 });
