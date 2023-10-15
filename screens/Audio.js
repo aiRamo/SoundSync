915c3b4c3c0be8713b1e1ee4995a309edcee4a2c
edgarh <edgar.hernandez4@mavs.uta.edu>
2023-10-15 16:09:33 -0500

    port change

--- screens/Audio.js
+++ screens/Audio.js
@@ -1,33 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import WebSocket from 'react-native-websocket';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: '',
-    };
-  }
+export default function AudioRecorder() {
+  const [message, setMessage] = useState('');
 
-  handleData(data) {
+  const handleData = (data) => {
     // Handle incoming WebSocket data here
+    setMessage(data);
+  };
 
-    this.setState({ message: data });
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Received Message: {message}</Text>
+      <WebSocket
+        url="ws://192.168.1.108:7681" // Replace with your RPI's IP address and port
+        onMessage={(message) => handleData(message)}
+        onError={(error) => console.log('Error:', error)}
+        onClose={(event) => console.log('Closed:', event)}
+        reconnect // Enable auto-reconnect
+      />
+    </View>
+  );
   }
-
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Received Message: {this.state.message}</Text>
-        <WebSocket
-          url="ws://192.168.1.108:9999" // Replace with your RPI's IP address and port
-          onMessage={this.handleData.bind(this)}
-          onError={(error) => console.log('Error:', error)}
-          onClose={(event) => console.log('Closed:', event)}
-          reconnect // Enable auto-reconnect
-        />
-      </View>
-    );
-  }
-}
