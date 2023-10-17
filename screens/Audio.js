06e265d7b7d77e7dd68e032f197429e2e8564feb
edgarh <edgar.hernandez4@mavs.uta.edu>
2023-10-17 02:30:35 -0500

    working client

--- screens/Audio.js
+++ screens/Audio.js
@@ -1,48 +1,28 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, ScrollView } from 'react-native';
-import WebSocket from 'react-native-websocket';
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-export default function AudioRecorder() {
-  const [messages, setMessages] = useState([]);
-  const [isConnected, setIsConnected] = useState(false);
-  const scrollViewRef = useRef();
-
-  const handleData = (data) => {
-    // Handle incoming WebSocket data here
-    setMessages((prevMessages) => [...prevMessages, data]);
-    console.log('Received: ', data);
-  };
-
-  const handleOpen = () => {
-    setIsConnected(true);
-    console.log('WebSocket connected successfully.');
-  };
+export default function AudioRecorder(){
+  const [lastMessage, setLastMessage] = useState('No messages yet');
 
   useEffect(() => {
-    // Whenever messages change, scroll to the bottom of the ScrollView
-    scrollViewRef.current.scrollToEnd({ animated: true });
-  }, [messages]);
+    // Set up a WebSocket connection to your server
+    const ws = new WebSocket('ws://192.168.1.108:9000');
+
+    // Handle received messages
+    ws.onmessage = (event) => {
+      setLastMessage(event.data);
+    };
+
+    return () => {
+      // Close the WebSocket connection when unmounting the component
+      ws.close();
+    };
+  }, []);
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>WebSocket Terminal:</Text>
-      <Text>{isConnected ? 'Connected' : 'Connecting...'}</Text>
-      <ScrollView
-        ref={scrollViewRef}
-        style={{ flex: 1, borderColor: 'gray', borderWidth: 1, marginBottom: 20 }}
-      >
-        {messages.map((message, index) => (
-          <Text key={index}>{message}</Text>
-        ))}
-      </ScrollView>
-      <WebSocket
-        url="ws://192.168.15.111:7681" // Replace with your RPI's IP address and port
-        onMessage={(message) => handleData(message)}
-        onOpen={handleOpen} // Handle the WebSocket connection success
-        onError={(error) => console.log('Error:', error)}
-        onClose={(event) => console.log('Closed:', event)}
-        reconnect // Enable auto-reconnect
-      />
+      <Text>{lastMessage}</Text>
     </View>
   );
-}
+};
