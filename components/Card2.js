0297402c47368a48f3ceab6f0317f44bb9ca5a2a
Benjamin <benfarm17@gmail.com>
2023-11-21 14:36:18 -0600

    delete functionality

--- components/Card2.js
+++ components/Card2.js
@@ -3,7 +3,11 @@ import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
 import { Entypo } from "@expo/vector-icons";
 import React from "react";
 
-export default function Card2({ title }) {
+export default function Card2({
+  title,
+  handleSubfolderSelection,
+  handleDelete,
+}) {
   return (
     <View>
       <View
@@ -16,6 +20,40 @@ export default function Card2({ title }) {
         <View style={styles.container2}>
           <Entypo name="folder" size={24} color="darkslateblue" />
           <Text style={{ marginLeft: 10 }}>{title}</Text>
+          <View
+            style={{
+              flexDirection: "row",
+              flex: 1,
+              justifyContent: "flex-end",
+              marginRight: 20,
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                marginLeft: 20,
+                backgroundColor: "darkslateblue",
+
+                borderRadius: 10,
+                borderWidth: 1,
+                padding: 5,
+              }}
+              onPress={handleSubfolderSelection}
+            >
+              <Text style={{ color: "white" }}>Select</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                marginLeft: 20,
+                backgroundColor: "red",
+                borderRadius: 10,
+                borderWidth: 1,
+                padding: 5,
+              }}
+              onPress={handleDelete}
+            >
+              <Text style={{ color: "white" }}>Delete</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     </View>
